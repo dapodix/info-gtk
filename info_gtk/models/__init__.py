c645ff503f03be37783c096b4ecfdd801f81c3cb
hexatester <revolusi147id@gmail.com>
2020-12-22 12:21:50 +0700

    add VerivikasiDataTunjangan

--- info_gtk/models/__init__.py
+++ info_gtk/models/__init__.py
@@ -2,10 +2,12 @@ from .data_individu import DataIndividu
 from .status_nuptk import StatusNuptk
 from .kelulusan_sertifikasi import KelulusanSertifikasi
 from .rombongan_belajar import RombonganBelajar
+from .verivikasi_data_tunjangan import VerivikasiDataTunjangan
 
 __all__ = [
     "DataIndividu",
     "StatusNuptk",
     "KelulusanSertifikasi",
     "RombonganBelajar",
+    "VerivikasiDataTunjangan",
 ]
---
+++ info_gtk/models/verivikasi_data_tunjangan.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class VerivikasiDataTunjangan:
+    nuptk: bool
+    keterangan_nuptk: str
+    beban_mengajar: bool
+    keterangan_beban_mengajar: str
+    kelengkapan_data: bool
+    keterangan_kelengkapan_data: str
+    kelulusan_sertifikasi: bool
+    keterangan_kelulusan_sertifikasi: str
+    keaktifan: bool
+    keterangan_keaktifan: str
+    kepegawaian: bool
+    keterangan_kepegawaian: str
+    usia: bool
+    keterangan_usia: str
