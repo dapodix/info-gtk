e421cea229ab7ea0b5b25cd660b998f25bd16ad1
hexatester <revolusi147id@gmail.com>
2020-12-22 12:11:53 +0700

    add InfoGtk

--- info_gtk/__init__.py
+++ info_gtk/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-from .version import __version__
-from .base import BaseInfoGtk
+from .version import __version__  # NOQA
+from .base import BaseInfoGtk  # NOQA
 from .auth import AuthInfoGtk
 from logging import getLogger
 from requests import Session
@@ -8,17 +8,7 @@ from requests import Session
 
 class InfoGtk(AuthInfoGtk):
     def __init__(self, email: str, password: str):
-        self.logger = getLogger(self.__class__.__name__)
+        self._logger = getLogger(self.__class__.__name__)
         self._email = email
         self._password = password
-        self.session = Session()
-
-    def _dashboard(self, retry=0) -> str:
-        res = self.session.get(self.BASE_URL + "dashboard")
-        if not res.ok:
-            self.logger.info("Dashboard failed")
-            if retry > 0:
-                return self.login(self._email, self._password, retry)
-            return ""
-        self.logger.info("Dashboard success")
-        return res.text
+        self._session = Session()
--- info_gtk/auth.py
+++ info_gtk/auth.py
@@ -5,10 +5,10 @@ class AuthInfoGtk(BaseInfoGtk):
     def login(self, email: str = None, password: str = None, retry=0) -> bool:
         email = email or self._email
         password = password or self._password
-        self.logger.debug("Getting login page")
-        res = self.session.get(self.BASE_URL + "/?s=999&pesan=")
+        self._logger.debug("Getting login page")
+        res = self._session.get(self.BASE_URL + "/?s=999&pesan=")
         if not res.ok:
-            self.logger.debug("Getting login page failed")
+            self._logger.debug("Getting login page failed")
             if retry > 0:
                 return self.login(email, password, retry)
             return False
@@ -21,16 +21,16 @@ class AuthInfoGtk(BaseInfoGtk):
             "metode": "Account",
             "s": "990",
         }
-        self.logger.debug("Trying to login")
-        res = self.session.post(self.BASE_URL, data=data, allow_redirects=False)
+        self._logger.debug("Trying to login")
+        res = self._session.post(self.BASE_URL, data=data, allow_redirects=False)
         if not res.status_code == 302:
-            self.logger.debug("Login failed")
+            self._logger.debug("Login failed")
             if retry > 0:
                 return self.login(email, password, retry)
             return False
-        self.logger.debug("Login success")
+        self._logger.debug("Login success")
         return res.status_code == 302
 
     def logout(self) -> bool:
-        res = self.session.get(self.BASE_URL + "auth/logout")
+        res = self._session.get(self.BASE_URL + "auth/logout")
         return res.ok
