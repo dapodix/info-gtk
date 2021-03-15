ec15275ed528c77cade3b8887b2dccbcb06f8f34
hexatester <revolusi147id@gmail.com>
2021-03-15 20:07:27 +0700

    Move models to root

--- info_gtk/__init__.py
+++ info_gtk/__init__.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
-from .version import __version__  # NOQA
+from .data_individu import DataIndividu
+from .status_nuptk import StatusNuptk
+from .kelulusan_sertifikasi import KelulusanSertifikasi
+from .realisasi_sktp import RealisasiSktp
+from .profile import Profile
+from .rombongan_belajar import RombonganBelajar
+from .sktp import Sktp
+from .verivikasi_data_tunjangan import VerivikasiDataTunjangan
+
+
 from .info_gtk import InfoGtk
 
-__all__ = ["InfoGtk"]
+from .version import __version__  # NOQA
+
+
+__all__ = [
+    "DataIndividu",
+    "StatusNuptk",
+    "KelulusanSertifikasi",
+    "RealisasiSktp",
+    "Profile",
+    "RombonganBelajar",
+    "Sktp",
+    "VerivikasiDataTunjangan",
+    "InfoGtk",
+]
