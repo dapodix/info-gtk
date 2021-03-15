c65d40fba90121e414d1275a3bfe2126c376cf1e
hexatester <revolusi147id@gmail.com>
2021-03-15 20:54:50 +0700

    Add test_info_gtk

--- tests/test_info_gtk.py
+++ tests/test_info_gtk.py
@@ -1,5 +1,10 @@
-from info_gtk import __version__
+from info_gtk import __version__, InfoGtk
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_info_gtk(email: str, password: str):
+    info_gtk = InfoGtk(email, password)
+    assert info_gtk.is_login is True
