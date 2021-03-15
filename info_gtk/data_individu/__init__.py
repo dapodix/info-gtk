279ed54f61c828b4ab33fe597070d63056e1b2ce
hexatester <revolusi147id@gmail.com>
2021-03-15 20:14:45 +0700

    Refactor DataIndividu

--- info_gtk/data_individu/__init__.py
+++ info_gtk/data_individu/__init__.py
@@ -1,35 +1,9 @@
-from __future__ import annotations
-from bs4 import Tag
-from dataclasses import dataclass
 from .ijazah_terakhir import IjazahTerakhir
 from .jam_mengajar import JamMengajar, JamMengajarInduk, JamMengajarNonInduk
 from .sekolah_induk import SekolahInduk, LokasiSekolahInduk
 from .status_kepegawaian import StatusKepegawaian
 from .tugas_tambahan import TugasTambahan
-
-
-@dataclass
-class DataIndividu:
-    update_terakhir: str
-    nuptk: str
-    nama: str
-    tanggal_lahir: str
-    nik: int
-    email: str
-    tanggal_pensiun: str
-    jenis_ptk: str
-    status_kepegawaian: StatusKepegawaian
-    ijazah_terakhir: IjazahTerakhir
-    sekolah_induk: SekolahInduk
-    lokasi_sekolah_induk: LokasiSekolahInduk
-    tugas_tambahan: TugasTambahan
-    jumlah_jam_mengajar: JamMengajar
-    total_jjm_linier_tambahan: str
-
-    @classmethod
-    def from_fit_individu(cls, tag: Tag) -> DataIndividu:
-        # #fit_individu
-        pass
+from .data_individu import DataIndividu
 
 
 __all__ = [
