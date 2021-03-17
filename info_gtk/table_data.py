a9603bb4305be668e3a0ca41d99a794d0dc62a31
hexatester <revolusi147id@gmail.com>
2021-03-17 21:06:31 +0700

    Add TableData.make_status_nuptk

--- info_gtk/table_data.py
+++ info_gtk/table_data.py
@@ -44,3 +44,20 @@ class TableData:
         if not tabledata:
             return list()
         return cls.from_json5_list(tabledata)
+
+    @classmethod
+    def make_status_nuptk(cls, data: str) -> List["TableData"]:
+        tabledata: str = ""
+        datas = data.splitlines()
+        for i, line in enumerate(datas):
+            if "/*arsip NUPTK*/" in line:
+                tabledata = datas[i + 1]
+                tabledata = tabledata.strip()
+                tabledata = tabledata.lstrip("try { var tabledata = ")
+                tabledata = tabledata.rstrip(
+                    ';putTable("#fit_ArsipNuptk",tabledata);} catch(err) {  };'
+                )
+                break
+        if not tabledata:
+            return list()
+        return cls.from_json5_list(tabledata)
