b1e697c2946a5f6c01a7225e3872ab50f1db7a90
hexatester <revolusi147id@gmail.com>
2021-03-17 21:08:09 +0700

    Add StatusNuptk.from_table_data

--- info_gtk/status_nuptk.py
+++ info_gtk/status_nuptk.py
@@ -1,5 +1,7 @@
 import attr
-from bs4 import Tag
+from typing import List
+
+from info_gtk.table_data import TableData
 
 
 @attr.dataclass(slots=True)
@@ -15,6 +17,15 @@ class StatusNuptk:
     kab_kota: str
 
     @classmethod
-    def from_fit_arsip_nuptk(cls, tag: Tag) -> "StatusNuptk":
-        # #fit_ArsipNuptk
-        pass
+    def from_table_data(cls, table_data: List[TableData]) -> "StatusNuptk":
+        return cls(
+            nuptk=table_data[0].data,
+            nama=table_data[1].data,
+            tempat_lahir=table_data[2].data,
+            tanggal_lahir=table_data[3].data,
+            nama_ibu_kandung=table_data[4].data,
+            nip=table_data[5].data,
+            nik=table_data[6].data,
+            instansi=table_data[7].data,
+            kab_kota=table_data[8].data,
+        )
