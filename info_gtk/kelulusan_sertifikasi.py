690ee0ac154828a64a301f4830bc38ea8be408bc
hexatester <revolusi147id@gmail.com>
2021-03-18 09:51:10 +0700

    Initial KelulusanSertifikasi.from_table_data

--- info_gtk/kelulusan_sertifikasi.py
+++ info_gtk/kelulusan_sertifikasi.py
@@ -1,5 +1,7 @@
 import attr
-from bs4 import Tag
+from typing import List
+
+from .table_data import TableData
 
 
 @attr.dataclass(slots=True)
@@ -17,6 +19,17 @@ class KelulusanSertifikasi:
     kab_kota_saat_ini: str
 
     @classmethod
-    def from_fit_kelulusan(cls, tag: Tag) -> "KelulusanSertifikasi":
-        # #fit_Kelulusan
-        pass
+    def from_table_data(cls, tds: List[TableData]) -> "KelulusanSertifikasi":
+        return cls(
+            nama=tds[0].data,
+            tanggal_lahir=tds[1].data,
+            nuptk=tds[2].data,
+            nrg=tds[3].data,
+            nomor_peserta=tds[4].data,
+            nomor_sertifikat=tds[5].data,
+            kode_sertifikasi_pertama=tds[6].data,
+            nama_bidang_studi_sertifikasi=tds[7].data,
+            nama_sekolah_terbit_nrg=tds[8].data,
+            data_tercatat_jenjang=tds[9].data,
+            kab_kota_saat_ini=tds[10].data,
+        )
